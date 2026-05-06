2feb4baa9dc29217700a3d30211fe7aa4a709960
Anders Eriksson <anteman.swe@gmail.com>
2026-05-06 16:11:50 +0200

    Forgotten parentesis

--- kino-webb/src/app/page.tsx
+++ kino-webb/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div>
       <main>
         <div>
-          <h1 className={styles.headline}>Kino - superprojektet  med Next.js (Page.tsx</h1>
+          <h1 className={styles.headline}>Kino - superprojektet  med Next.js (Page.tsx)</h1>
           <Start />
         </div>
       </main>
