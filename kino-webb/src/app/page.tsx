eb57686a14f4bf32a8b77d93b88b7c389a96b1ef
elinmolindr <elinmolinder1@gmail.com>
2026-05-12 20:04:52 +0200

    Add Header component with styling (#42)
    * Add Header component with styling
    
    * Moved Header to page.tsx and removed unnecessary @use imports according to review.

--- kino-webb/src/app/page.tsx
+++ kino-webb/src/app/page.tsx
@@ -1,10 +1,12 @@
 
 import styles from "./page.module.scss";
 import Start from "./components/start";
+import Header from "./components/header/Header";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <main>
         <div>
           <h1 className={styles.headline}>Kino - superprojektet  med Next.js (Page.tsx)</h1>
