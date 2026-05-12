c5d409a2fdea18f9876398ffa1a3a4719a80177a
elinmolindr <elinmolinder1@gmail.com>
2026-05-12 20:04:52 +0200

    Add Header component with styling (#42)
    * Add Header component with styling
    
    * Moved Header to page.tsx and removed unnecessary @use imports according to review.

---
+++ kino-webb/src/app/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.scss";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__inner}>
+        {/* LOGO, LOGIN_BTN etc */}
+      </div>
+    </header>
+  );
+}
--- kino-webb/src/app/layout.tsx
+++ kino-webb/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.scss";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,7 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body className="root">{children}</body>
+      <body className="root">
+  
+          {children}</body>
     </html>
   );
 }
