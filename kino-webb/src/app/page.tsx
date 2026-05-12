9904795aad01a8d774541e675aa338982d6677a9
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-05-12 19:16:14 +0200

    move footer markup from page to footer component

--- kino-webb/src/app/page.tsx
+++ kino-webb/src/app/page.tsx
@@ -1,66 +1,18 @@
 
 import styles from "./page.module.scss";
 import Start from "./components/start";
+import Footer from "./components/footer/Footer";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.page}>
       <main>
         <h1 className={styles.headline}>
           Kino - superprojekt
         </h1>
       </main>
-
-      <footer className={styles.footer}>
-  <div className={styles.footerContent}>
-    <section className={styles.footerSection}>
-      <h3>Om oss</h3>
-      <ul className={styles.footerLinks}>
-        <li><a href="#">Jobba hos oss</a></li>
-        <li><a href="#">Press</a></li>
-        <li><a href="#">Om Kino</a></li>
-        <li><a href="#">Filmfestivalen</a></li>
-        <li><a href="#">Hitta hit</a></li>
-      </ul>
-    </section>
-
-    <section className={styles.footerSection}>
-      <h3>Övrigt</h3>
-      <ul className={styles.footerTerms}>
-        <li><a href="#">Köpvillkor</a></li>
-        <li><a href="#">Integritetspolicy</a></li>
-        <li><a href="#">Åldersgränser</a></li>
-        <li><a href="#">Tillgänglighet</a></li>
-      </ul>
-    </section>
-
-    <section className={styles.footerSection}>
-      <h3>Hjälp & Kontakt</h3>
-      <ul className={styles.footerInfo}>
-        <li><a href="#">Storgatan 12, 123 45 Lycksele</a></li>
-        <li><a href="#">+46 950-xxxxxxx</a></li>
-        <li><a href="#">info@kino.com</a></li>
-      </ul>
-    </section>
-
-    <section className={styles.footerSection}>
-      <h3>Sociala medier</h3>
-      <div className={styles.footerIcons}>
-        <a href="#">𝕏</a>
-        <a href="#">f</a>
-        <a href="#">◎</a>
-      </div>
-    </section>
-  </div>
-
-  <section className={styles.footerExtra}>
-    <h3>Nyhetsbrev</h3>
-    <form className={styles.newsletter}>
-      <input className={styles.newsletterInput} placeholder="you@example.com" />
-      <button className={styles.newsletterSubmit}>Anmäl dig</button>
-    </form>
-  </section>
-</footer>
+      <Start />
+      <Footer />
     </div>
-  );
+  );  
 }
