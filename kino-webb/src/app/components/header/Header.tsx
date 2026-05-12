17a3504083c19678518406d71f12f874f83aa6dc
elinmolindr <elinmolinder1@gmail.com>
2026-05-12 21:26:33 +0200

    Add Logo component and update header structure

--- kino-webb/src/app/components/header/Header.tsx
+++ kino-webb/src/app/components/header/Header.tsx
@@ -1,10 +1,14 @@
 import styles from "./Header.module.scss";
+import Logo from "./Logo";
 
 export default function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.header__inner}>
-        {/* LOGO, LOGIN_BTN etc */}
+         
+        
+          <Logo />
+        
       </div>
     </header>
   );
