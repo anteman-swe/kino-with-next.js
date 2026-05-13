9e150fa6ca053afdcd60ec5c08fff0c521c1ef25
AntemanSWE <anteman.swe@gmail.com>
2026-05-13 14:03:41 +0200

    Anteman swe/documentation (#49)
    * Connected root script to kino-webb (#1)
    
    * Sass support (#7)
    
    * Fix compiling error in Next.js, add API-routes to start with.  Empty page.tsx and styling (#2)
    
    * add support for sass
    
    * Complete support for sass
    
    * Forgotten parentesis
    
    * Setup Prisma, Prisma-client and secure system with .env (#11)
    
    * Adaptations (#40)
    
    * adaption with sass prependData in preparation for components
    
    * Fix: docker-compose.yaml now pointing to correct env-file
    
    * Update with Base-UI  in project
    
    * install Auth.js, basic setup
    
    * install Auth.js, basic setup
    
    * Expanded User-model for db
    
    * Update prisma schema
    
    * Add auth.ts and implement necessary functions
    
    * Add Zod to Auth to validate credentials with schema
    
    * Update imports
    
    * Change script name
    
    * Update README with instructions to get started

--- kino-webb/src/app/components/start.tsx
+++ kino-webb/src/app/components/start.tsx
@@ -1,7 +1,9 @@
+import './start module.scss';
+
 export default function Start() {
   return (
     <div>
-      <h1>Start Module</h1>
+      <h1>Start Module - temporary...</h1>
     </div>
   );
 }
---
+++ kino-webb/src/auth.ts
@@ -0,0 +1,42 @@
+import NextAuth from "next-auth";
+import { ZodError } from "zod";
+import Credentials from "next-auth/providers/credentials";
+import { signInSchema } from "./app/lib/zod";
+import { saltAndHashPassword } from '@/src/app/utils/password';
+import { getUserFromDb } from "./app/utils/db";
+
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "E-post", type: "email", placeholder: "exempelnamn@exempel.se"},
+        password: { label: "Lösenord", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) return null; // TODO: Behövs annan felhantering?
+        try {
+          let user = null;
+
+          const { email, password } = await signInSchema.parseAsync(credentials);
+
+          const pwHash = await saltAndHashPassword(password);
+
+          user = await getUserFromDb(email, pwHash);
+
+          if (!user) return null;
+
+          return user;
+
+        } catch (error) {
+          if (error instanceof ZodError) {
+            return null;
+          }
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt"
+  },
+})
