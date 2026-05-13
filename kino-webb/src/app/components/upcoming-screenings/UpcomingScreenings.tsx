7ad4a2dcb543d242ff1450c43dc692a406af6611
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-05-13 15:50:31 +0200

    Connect component upcoming-screenings to dummy data

--- kino-webb/src/app/components/upcoming-screenings/UpcomingScreenings.tsx
+++ kino-webb/src/app/components/upcoming-screenings/UpcomingScreenings.tsx
@@ -1,35 +1,51 @@
 import style from './UpcomingScreenings.module.scss';
+import { screenings } from "../../../Data/screenings";
+import { movies } from "../../../Data/movies";
 
-export default function UpcomingScreenings() {  
-    return (    
-        <div className={style.upcomingScreenings}>
-            <h2 className={style.upcomingScreeningsHeading}>Kommande visningar</h2>
-            <div className={style.upcomingScreeningsTrack}>
-                <div className={style.upcomingScreeningsCard}>
-                    <img className={style.upcomingScreeningsImg} src="/film1.jpg" alt="Film 1" />
-                    <h4 className={style.upcomingScreeningsTitle}>Film 1</h4>
-                    <p className={style.upcomingScreeningsTime}>18:00</p>
-                    <p className={style.upcomingScreeningsRoom}>Salong 1</p>
-                </div>
-                <div className={style.upcomingScreeningsCard}>
-                    <img className={style.upcomingScreeningsImg} src="/film2.jpg" alt="Film 2" />
-                    <h4 className={style.upcomingScreeningsTitle}>Film 2</h4>
-                    <p className={style.upcomingScreeningsTime}>20:30</p>
-                    <p className={style.upcomingScreeningsRoom}>Salong 2</p>
-                </div>
-                <div className={style.upcomingScreeningsCard}>
-                    <img className={style.upcomingScreeningsImg} src="/film3.jpg" alt="Film 3" />
-                    <h4 className={style.upcomingScreeningsTitle}>Film 3</h4>       
-                    <p className={style.upcomingScreeningsTime}>21:00</p>
-                    <p className={style.upcomingScreeningsRoom}>Salong 3</p>
-                </div>
-                <div className={style.upcomingScreeningsCard}>
-                    <img className={style.upcomingScreeningsImg} src="/film4.jpg" alt="Film 4" />
-                    <h4 className={style.upcomingScreeningsTitle}>Film 4</h4>       
-                    <p className={style.upcomingScreeningsTime}>22:00</p>
-                    <p className={style.upcomingScreeningsRoom}>Salong 4</p>
-                </div>
-            </div>
-        </div>
-    );
+export default function UpcomingScreenings() {
+  const upcomingScreenings = screenings.map((screening) => {
+    const movie = movies.find((movie) => movie.id === screening.movieId);
+
+    return {
+      ...screening,
+      movie,
+    };
+  });
+
+  return (
+    <div className={style.upcomingScreenings}>
+      <h2 className={style.upcomingScreeningsHeading}>Kommande visningar</h2>
+
+      <div className={style.upcomingScreeningsTrack}>
+        {upcomingScreenings.map((screening) => (
+          <div className={style.upcomingScreeningsCard} key={screening.id}>
+            <img
+              className={style.upcomingScreeningsImg}
+              src={screening.movie?.Poster_Link}
+              alt={screening.movie?.Series_Title}
+            />
+
+            <h3 className={style.upcomingScreeningsTitle}>
+              {screening.movie?.Series_Title}
+            </h3>
+
+            <p className={style.upcomingScreeningsTime}>
+              {new Date(screening.startsAt).toLocaleTimeString("sv-SE", {
+                hour: "2-digit",
+                minute: "2-digit",
+              })}
+            </p>
+
+            <p className={style.upcomingScreeningsRoom}>
+              {screening.auditorium}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
+
+
+
+
