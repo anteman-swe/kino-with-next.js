f9be05ff9627c358f1da1c69f5c1c3c3897f7120
Anders Eriksson <anteman.swe@gmail.com>
2026-05-19 14:28:27 +0200

    Add passwordHash to dummy-users

--- kino-webb/src/Data/users.ts
+++ kino-webb/src/Data/users.ts
@@ -3,12 +3,14 @@ export const users = [
     id: 1,
     name: "Guy McDudesson",
     email: "Guy@example.com",
+    passwordHash: "",
     role: "CUSTOMER",
   },
     {
     id: 2,
     name: "Admin",
     email: "admin@example.com",
+    passwordHash: "",
     role: "ADMIN",
   },
 ];
