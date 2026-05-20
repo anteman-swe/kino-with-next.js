e3926797163b157d859ffeb83ba416781b725f3c
Anders Eriksson <anteman.swe@gmail.com>
2026-05-20 19:52:27 +0200

    Adaptations between schemas and database for all tables to get

--- kino-webb/prisma/seed.ts
+++ kino-webb/prisma/seed.ts
@@ -7,7 +7,6 @@ import { bookings } from '@/Data/bookings';
 import { movies } from '@/Data/movies';
 import { screenings } from '@/Data/screenings';
 
-
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
