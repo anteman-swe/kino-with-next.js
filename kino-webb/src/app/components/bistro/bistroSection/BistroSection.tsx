fe97a916305d6a7eeee98267648690e2336c24d9
elinmolindr <elinmolinder1@gmail.com>
2026-05-21 18:33:21 +0200

    Add BistroBanner component (#63)

--- kino-webb/src/app/components/bistro/bistroSection/BistroSection.tsx
+++ kino-webb/src/app/components/bistro/bistroSection/BistroSection.tsx
@@ -1,12 +1,12 @@
 import styles from "./BistroSection.module.scss";
-
+import BistroBanner from "../bistroBanner/BistroBanner";
 export default function BistroSection() {
   return (
     <div className={styles.wrapper}>
       <h1 className={styles.heading}>Café&Bistro</h1>
 
       <section className={styles.section}>
-  
+         <BistroBanner />
       </section>
     </div>
   );
