d634baacdc040b4ae6a27397dec8c040d0b2425d
waqjel <42685048+waqjel@users.noreply.github.com>
2026-05-21 12:24:19 +0200

    Added Carousel files to the project (#62)
    * Added Carousel files to the project
    
    * Update MovieCarousel.tsx

--- kino-webb/src/app/page.tsx
+++ kino-webb/src/app/page.tsx
@@ -20,14 +20,12 @@ export default function Home() {
           <MovieCarousel movies={movies} />
         </section>
 
-         <PopularMovies />
+        <PopularMovies />
 
-      <UpcomingScreenings />
-
+        <UpcomingScreenings />
        
         <MovieList movies={movies} title="Aktuella filmer" />
         
-
         <BistroSection />
       </main>
     </>
