03023223b288c207c019b88f3de2b2991fd095e9
elinmolindr <elinmolinder1@gmail.com>
2026-05-21 12:04:59 +0200

    Add bistroSection component with layout and basic styling (#61)

--- kino-webb/src/app/page.tsx
+++ kino-webb/src/app/page.tsx
@@ -1,11 +1,9 @@
 import styles from "./page.module.scss";
-import Start from "./components/start";
 import MovieList from "./components/MovieList/MovieList";
 import { movies } from "../Data/movies";
 import UpcomingScreenings from "./components/upcoming-screenings/UpcomingScreenings";
 import MovieCarousel from "./components/movieCarousel/MovieCarousel";
 import BistroSection from "./components/bistro/bistroSection/BistroSection";
-
 export default function Home() {
   return (
     <>
