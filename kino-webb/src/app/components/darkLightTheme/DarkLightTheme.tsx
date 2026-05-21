75b3639631dc86bdfd51396da69edd85760f2d9a
Anders Eriksson <anteman.swe@gmail.com>
2026-05-21 18:20:19 +0200

    Renamed scss file with 'module'

--- kino-webb/src/app/components/darkLightTheme/DarkLightTheme.tsx
+++ kino-webb/src/app/components/darkLightTheme/DarkLightTheme.tsx
@@ -3,7 +3,7 @@
 import * as React from "react";
 import { useTheme } from "next-themes";
 import { Toggle } from "@base-ui/react/toggle";
-import "./DarkLightTheme.scss";
+import "./DarkLightTheme.module.scss";
 
 export default function DarkLightTheme() {
   const { setTheme, resolvedTheme } = useTheme();
