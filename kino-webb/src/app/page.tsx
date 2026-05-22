594e98afed8c14841ddb1d08f301f14517d854b6
Andreas Isaksson <aisaksson187@gmail.com>
2026-05-22 07:50:46 +0200

    Feature/movie card + Movie list (#52)
    * Changed moviecards from JS to be compatible with react
    
    * Added a few lines to make sure cards and lists with cards are created correctly
    
    * Fixed variables/mixins and added clearer styling to the moviecards
    
    * Fixed unnecessary misins used in .tsx
    
    * fixed page.tsx

--- kino-webb/src/app/page.tsx
+++ kino-webb/src/app/page.tsx
@@ -1,9 +1,11 @@
 import styles from "./page.module.scss";
+import Start from "./components/start";
+import MovieList from "./components/MovieList/MovieList";
+import { movies } from "../Data/movies";
 import UpcomingScreenings from "./components/upcoming-screenings/UpcomingScreenings";
-import { movies } from "@/Data/movies";
 import MovieCarousel from "./components/movieCarousel/MovieCarousel";
-
 import BistroSection from "./components/bistro/bistroSection/BistroSection";
+
 export default function Home() {
   return (
     <>
@@ -11,6 +13,7 @@ export default function Home() {
         <h1 className={styles.headline}>
           Kino - superprojekt
         </h1>
+        <MovieList movies={movies} title="Aktuella filmer" />
 
       <UpcomingScreenings />
     
