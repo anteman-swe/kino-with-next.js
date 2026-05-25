3c737afba7fe34bce634a0b399118b563389ee8c
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-05-25 17:00:14 +0200

    Add popular movies section (#66)
    * Add popular movies component and styling
    
    * Add popular movies section to homepage
    
    * Add mock reviews data
    
    * Update home page layout structure
    
    * Fix review feedback for popular movies component
    
    * Update depreceated setting in next.config
    
    * Fix:  note from Next about image sizing
    
    ---------
    
    Co-authored-by: Anders Eriksson <anteman.swe@gmail.com>

--- kino-webb/next.config.ts
+++ kino-webb/next.config.ts
@@ -5,6 +5,17 @@ const nextConfig: NextConfig = {
   sassOptions: {
     prependData: `@use "@/styles/variables.scss" as *; @use "@/styles/mixins.scss" as *;`,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+        port: '',
+        pathname: '*/**',
+      },
+    ],
+  },
+
   turbopack: {
     root: path.resolve(__dirname),
     // root: '/',
--- kino-webb/src/app/page.tsx
+++ kino-webb/src/app/page.tsx
@@ -1,26 +1,32 @@
 import styles from "./page.module.scss";
-import Start from "./components/start";
 import MovieList from "./components/MovieList/MovieList";
 import { movies } from "../Data/movies";
 import UpcomingScreenings from "./components/upcoming-screenings/UpcomingScreenings";
 import MovieCarousel from "./components/movieCarousel/MovieCarousel";
 import BistroSection from "./components/bistro/bistroSection/BistroSection";
+import PopularMovies from "./components/popular-movies/PopularMovies";
+
 
 export default function Home() {
   return (
     <>
       <main className={styles.page}>
+       
         <h1 className={styles.headline}>
           Kino - superprojekt
         </h1>
-        <MovieList movies={movies} title="Aktuella filmer" />
 
-      <UpcomingScreenings />
-    
-  
         <section className={styles.carouselSection}>
           <MovieCarousel movies={movies} />
         </section>
+
+         <PopularMovies />
+
+      <UpcomingScreenings />
+
+       
+        <MovieList movies={movies} title="Aktuella filmer" />
+        
 
         <BistroSection />
       </main>
