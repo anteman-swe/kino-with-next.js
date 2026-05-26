4aafc88f48cbfc3bed9748712df18e29b29e2339
Anders Eriksson <anteman.swe@gmail.com>
2026-05-26 09:01:55 +0200

    Improved logic in Header

--- kino-webb/src/app/components/header/Header.tsx
+++ kino-webb/src/app/components/header/Header.tsx
@@ -16,9 +16,7 @@ const logOut = async () => {await signOut({ redirectTo: "/" })};
 export default function Header({ onOpenLogin, children }: HeaderProps) {
   const { data: session, status } = useSession();
   const buttonMode = useMemo(() => {
-    if (session?.user && status === "authenticated") {
-      return true;
-    } else return false;
+    if (session?.user && status === "authenticated") { return true; } else return false;
   }, [session?.user, status]);
   
   return (
