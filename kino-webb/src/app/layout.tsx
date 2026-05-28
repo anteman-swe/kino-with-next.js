f0a423a0a796ca829faf3368616efe53d4e3f18b
Anders Eriksson <anteman.swe@gmail.com>
2026-05-28 12:00:03 +0200

    Added SessionProvider

--- kino-webb/src/app/layout.tsx
+++ kino-webb/src/app/layout.tsx
@@ -8,7 +8,6 @@ import { SessionProvider } from "next-auth/react";
 import HeaderWrapper from "./components/header/HeaderWrapper";
 import Footer from "./components/footer/Footer";
 
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -30,19 +29,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable}`}
+      suppressHydrationWarning
+      style={{ colorScheme: "light" }}
+    >
       <body className="root">
-        <ThemeProvider attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange>
-          <SessionProvider>
+        <SessionProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
             <HeaderWrapper />
             {children}
             <Footer />
-          </SessionProvider>
-        </ThemeProvider>
-        
+          </ThemeProvider>
+        </SessionProvider>
       </body>
     </html>
   );
