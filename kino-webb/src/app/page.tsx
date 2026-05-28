1bae75a1816f2f606bb51ac1538ef349d04ebcce
aisaksson187-create <aisaksson187@gmail.com>
2026-05-29 01:05:08 +0200

    Inserted the event list below bistro section

--- kino-webb/src/app/page.tsx
+++ kino-webb/src/app/page.tsx
@@ -1,34 +1,32 @@
 import styles from "./page.module.scss";
 import MovieList from "./components/MovieList/MovieList";
 import { movies } from "../Data/movies";
+import { events } from "../Data/event";
 import UpcomingScreenings from "./components/upcoming-screenings/UpcomingScreenings";
 import MovieCarousel from "./components/movieCarousel/MovieCarousel";
 import BistroSection from "./components/bistro/bistroSection/BistroSection";
 import PopularMovies from "./components/popular-movies/PopularMovies";
-
+import EventList from "./components/event/eventList";
 
 export default function Home() {
   return (
     <>
       <main className={styles.page}>
-       
-        <h1 className={styles.headline}>
-          Kino - superprojekt
-        </h1>
+        <h1 className={styles.headline}>Kino - superprojekt</h1>
 
         <section className={styles.carouselSection}>
           <MovieCarousel movies={movies} />
         </section>
 
-         <PopularMovies />
+        <PopularMovies />
 
-      <UpcomingScreenings />
+        <UpcomingScreenings />
 
-       
         <MovieList movies={movies} title="Aktuella filmer" />
-        
 
         <BistroSection />
+
+        <EventList events={events} />
       </main>
     </>
   );
