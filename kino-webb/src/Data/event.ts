d309a8792bbfc352ba3977a12a8501c732787352
Andreas Isaksson <aisaksson187@gmail.com>
2026-05-29 13:35:15 +0200

    Feature/event (#71)
    * Added images to represent the events
    
    * Added static data for the events
    
    * Added event tsx and styling
    
    * Inserted the event list below bistro section

---
+++ kino-webb/src/Data/event.ts
@@ -0,0 +1,22 @@
+import type { Event } from "../app/components/event/event";
+
+export const events: Event[] = [
+  {
+    id: "event-1",
+    title: "Hockey-VM på bioduken!",
+    date: "15-31 Maj 2026",
+    image: "/event1.jpg",
+  },
+  {
+    id: "event-2",
+    title: "Skräckvecka",
+    date: "28 Okt - 4 Nov 2026",
+    image: "/event2.jpg",
+  },
+  {
+    id: "event-3",
+    title: "Astrid Lindgren filmfest",
+    date: "5-12 juli 2026",
+    image: "/event3.jpg",
+  },
+];
