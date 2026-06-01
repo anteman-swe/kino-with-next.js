6d635f994323374e83a33da311dd27ccf5cf6eaa
Anders Eriksson <anteman.swe@gmail.com>
2026-06-01 12:49:31 +0200

    Updated API/movies/route with some error handling

--- kino-webb/src/app/api/movies/route.ts
+++ kino-webb/src/app/api/movies/route.ts
@@ -7,6 +7,16 @@ const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 export async function GET() {
-    const response = await prisma.movie.findMany({});
-    return NextResponse.json(response);
+    try {
+        const response = await prisma.movie.findMany({});
+        return NextResponse.json(response);
+    } catch(err) {
+        console.error('Error getting multiple movies from db:', err);
+        return NextResponse.json({
+            name: "dbError",
+            message: "Could not get any movies from DB",
+            status: 400
+        });
+    }
+    
 }
