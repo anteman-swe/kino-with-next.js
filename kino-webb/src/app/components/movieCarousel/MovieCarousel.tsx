315e39073fa46b4da27ae1f62760a7ab08995a1d
Anders Eriksson <anteman.swe@gmail.com>
2026-06-01 20:49:43 +0200

    Reversed module change

--- kino-webb/src/app/components/movieCarousel/MovieCarousel.tsx
+++ kino-webb/src/app/components/movieCarousel/MovieCarousel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import "./MovieCarousel.module.scss";
+import "./MovieCarousel.scss";
 
 export interface Movie {
   Certificate: string | number | null | undefined;
