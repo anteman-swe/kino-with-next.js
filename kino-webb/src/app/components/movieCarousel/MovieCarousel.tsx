a0fdec5a4293e644e35d7a8a0553c7de58ba1775
Anders Eriksson <anteman.swe@gmail.com>
2026-06-01 20:42:31 +0200

    Renamed to .module.scss

--- kino-webb/src/app/components/movieCarousel/MovieCarousel.tsx
+++ kino-webb/src/app/components/movieCarousel/MovieCarousel.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import "./MovieCarousel.scss";
+import "./MovieCarousel.module.scss";
 
 export interface Movie {
   Certificate: string | number | null | undefined;
