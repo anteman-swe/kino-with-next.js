e2b5efcd5f8de8097ef5dec1a2012b2ffff26141
AntemanSWE <anteman.swe@gmail.com>
2026-06-02 21:00:18 +0200

    Updated seed-function to include bookingSeats and corrected ordere of seeding to work with Relations. Added time zone inddicator to times for screenings cause db requirement (#92)

--- kino-webb/prisma/seed.ts
+++ kino-webb/prisma/seed.ts
@@ -1,14 +1,13 @@
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient, Role, BookStatus } from '@/generated/prisma/client';
-import { saltAndHashPassword } from '@/app/utils/password';
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient, Role, BookStatus } from "@/generated/prisma/client";
+import { saltAndHashPassword } from "@/app/utils/password";
 
 // importing dummy data
-import { bookings } from '@/Data/bookings';
-import { movies } from '@/Data/movies';
-import { screenings } from '@/Data/screenings';
-import { reviews } from '@/Data/reviews';
-import { offers } from '@/Data/offers';
-
+import { bookings } from "@/Data/bookings";
+import { movies } from "@/Data/movies";
+import { screenings } from "@/Data/screenings";
+import { reviews } from "@/Data/reviews";
+import { offers } from "@/Data/offers";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
@@ -17,19 +16,20 @@ async function main() {
   // Emptying the database before seeding to prevent duplicate data
   console.log("Tömmer databasen...");
   await prisma.user.deleteMany({});
-  await prisma.booking.deleteMany({});
   await prisma.movie.deleteMany({});
   await prisma.screening.deleteMany({});
   await prisma.review.deleteMany({});
   await prisma.offer.deleteMany({});
+  await prisma.booking.deleteMany({});
+  await prisma.bookingSeat.deleteMany({});
   console.log("Databasen är tömd!");
 
-  console.log("Genererar haschade lösenord...")
+  console.log("Genererar haschade lösenord...");
   // Making one password for use by both users
   const hashedPassword = await saltAndHashPassword("secret1234");
 
   // Creating one user with normal rights
-  console.log("Skapar vanlig användare...")
+  console.log("Skapar vanlig användare...");
   const normalUser = await prisma.user.create({
     data: {
       email: "guy@exempel.se",
@@ -37,10 +37,10 @@ async function main() {
       passwordHash: hashedPassword,
       role: "USER" as Role,
     },
-  })
+  });
 
   // Creating one user with admin rights
-  console.log("Skapar admin-användare...")
+  console.log("Skapar admin-användare...");
   const adminUser = await prisma.user.create({
     data: {
       email: "admin@exempel.se",
@@ -48,53 +48,111 @@ async function main() {
       passwordHash: hashedPassword,
       role: "ADMIN" as Role, // giving user admin rights
     },
-  })
+  });
 
-  console.log(`2 användare skapade!`)
-  console.log(`- Vanlig användare skapad: ${normalUser.email}`)
-  console.log(`- Admin-användare skapad: ${adminUser.email}`)
-  
+  console.log(`2 användare skapade!`);
+  console.log(`- Vanlig användare skapad: ${normalUser.email}`);
+  console.log(`- Admin-användare skapad: ${adminUser.email}`);
+
   // Creating the rest of the dummy-data
-  const bookingsWithEnumStatus = bookings.map(booking => ({
-    ...booking,
-    status: booking.status as BookStatus
-  }))
-  await prisma.booking.createMany({
-    data: bookingsWithEnumStatus,
-  })
-  console.log('Dummy-bokningar skrivna till databasen!');
-  
+  // Dummy data för movies konverteras för att passa till DB
+  const moviesToSeed = movies.map((movie) => ({
+    seriesTitle: movie.Series_Title,
+    releasedYear: movie.Released_Year,
+    genre: movie.Genre,
+    imdbRating: movie.IMDB_Rating,
+    director: movie.Director,
+    stars: movie.Stars,
+    runtime: movie.Runtime,
+    certificate: movie.Certificate,
+    overview: movie.Overview,
+    posterLink: movie.Poster_Link,
+    trailer: movie.Trailer,
+  }));
+  // Dummy movies skrivs till DB
   await prisma.movie.createMany({
-    data: movies,
-  })
-  console.log('Dummy data för filmer skrivna till databasen!');
+    data: moviesToSeed,
+  });
+  console.log("Dummy data för filmer skrivna till databasen!");
 
+  // Dummy data för visningar skrivs till DB
+  const movieList = await prisma.movie.findMany({});
+  const sortedMList = movieList.sort((a, b) => a.id - b.id);
+  const getRandomId = (): number => {
+    const randomIdInList =
+      Math.floor(Math.random() * sortedMList.length) + sortedMList[0].id;
+    return randomIdInList;
+  };
+
+  const adaptedScreenings = screenings.map((screening) => ({
+    ...screening,
+    movieId: getRandomId(),
+  }));
   await prisma.screening.createMany({
-    data: screenings,
-  })
+    data: adaptedScreenings,
+  });
+  console.log("Dummy data för visningar är skrivna till databasen!");
 
-  console.log('Dummy data för visningar är skrivna till databasen!');
+  const userList = await prisma.user.findMany({});
+  const sortedUserList = userList.sort((a, b) => a.id - b.id);
+  const getRandomUserId = (): number => {
+    const randomIdInList =
+      Math.floor(Math.random() * sortedUserList.length) + sortedUserList[0].id;
+    return randomIdInList;
+  };
 
-  
+  const adaptedReviews = reviews.map((review) => ({
+    ...review,
+    movieId: getRandomId(),
+    userId: getRandomUserId(),
+  }));
   await prisma.review.createMany({
-    data: reviews,
-  })
+    data: adaptedReviews,
+  });
 
-  console.log('Dummy data för visningar är skrivna till databasen!');
+  console.log("Dummy data för recensioner är skrivna till databasen!");
 
   await prisma.offer.createMany({
     data: offers,
-  })
-  console.log('Dummy data för erbjudanden är skrivna till databasen!');
+  });
+  console.log("Dummy data för erbjudanden är skrivna till databasen!");
 
-  console.log('Databasen är seedad för testning, KLART!')
+  // Dummy data för bookings skrivs till DB
+  const bookingsWithEnumStatus = bookings.map((booking) => ({
+    ...booking,
+    userId: getRandomUserId(),
+    status: booking.status as BookStatus,
+  }));
+  await prisma.booking.createMany({
+    data: bookingsWithEnumStatus,
+  });
+  console.log("Dummy-bokningar skrivna till databasen!");
+
+  // Dummy data för upptagna stolar
+  const registeredBookings = await prisma.booking.findMany({});
+
+  const bSeats = registeredBookings.flatMap((booking) => {
+    const seats = booking?.seats ?? [];
+    return seats.map((seat) => ({
+      bookingId: booking.id,
+      screeningId: booking.screeningId,
+      seat: seat,
+    }));
+  });
+
+  await prisma.bookingSeat.createMany({
+    data: bSeats,
+  });
+  console.log('Dummy data för bokade stolar är skrivna till databasen');
+
+  console.log("Databasen är seedad för testning, KLART!");
 }
 
 main()
   .catch((e) => {
-    console.error(e)
-    process.exit(1)
+    console.error(e);
+    process.exit(1);
   })
   .finally(async () => {
-    await prisma.$disconnect()
-  })
+    await prisma.$disconnect();
+  });
