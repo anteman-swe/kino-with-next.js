d772a1f60b17d68302d9620f5bf2f50717d02eb1
AntemanSWE <anteman.swe@gmail.com>
2026-06-04 13:22:53 +0200

    Cookiepolicy page, text from md-file. (#97)
    * Cookiepolicy page, text from md-file.
    
    * Added link in Logo and removed top link in cookiepolicy
    
    * Changed img tag to Image tag

---
+++ kino-webb/src/app/cookiepolicy/page.tsx
@@ -0,0 +1,31 @@
+import { marked } from 'marked';
+import { promises as fs } from 'fs';
+import path from 'path';
+import Link from 'next/link';
+import styles from './Cookiepolicy.module.scss';
+
+export default async function Cookiepolicy() {
+    const filePath = path.join(process.cwd(), 'public', 'kino-cookie-policy.md');
+
+    let cookiePolicy = '';
+
+    try {
+        cookiePolicy = await fs.readFile(filePath, 'utf-8');
+    } catch (error) {
+        console.error('Could not read file:', error);
+        cookiePolicy = 'Hoppsan, textinnehållet kunde inte läsas in...';
+    }
+    const policyToPresent = marked.parse(cookiePolicy);
+
+    return (
+      <>
+        <div
+          dangerouslySetInnerHTML={{ __html: policyToPresent }}
+          className={styles.cookiepolicy}
+        ></div>
+        <Link href="/" className={styles.homelink}>
+          Tillbaka till startsidan
+        </Link>
+      </>
+    );
+}
