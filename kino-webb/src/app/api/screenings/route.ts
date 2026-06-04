248589c4d7999c2664de2afad0403adc90d7885b
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-06-04 21:17:31 +0200

    Create screenings API endpoint

--- kino-webb/src/app/api/screenings/route.ts
+++ kino-webb/src/app/api/screenings/route.ts
@@ -1,5 +1,34 @@
-// import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
 
 export async function GET() {
-    return Response.json({message: 'Hello Movies!'});
+  try {
+    const screenings = await prisma.screening.findMany({
+      orderBy: {
+        startsAt: "asc",
+      },
+    });
+
+    const movieIds = screenings.map((screening) => screening.movieId);
+
+    const movies = await prisma.movie.findMany({
+      where: {
+        id: {
+          in: movieIds,
+        },
+      },
+    });
+
+    const screeningsWithMovies = screenings.map((screening) => ({
+      ...screening,
+      movie: movies.find((movie) => movie.id === screening.movieId),
+    }));
+
+    return NextResponse.json(screeningsWithMovies);
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong", error },
+      { status: 500 }
+    );
+  }
 }
