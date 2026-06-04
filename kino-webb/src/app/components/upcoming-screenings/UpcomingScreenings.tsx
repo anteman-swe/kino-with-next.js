b78c5e7f3f2310c291e14acacd68f2d962744921
LatifaBik <latifa.bikndarne@lerniastudent.se>
2026-06-04 21:20:03 +0200

    Connect upcoming screenings component to API

--- kino-webb/src/app/components/upcoming-screenings/UpcomingScreenings.tsx
+++ kino-webb/src/app/components/upcoming-screenings/UpcomingScreenings.tsx
@@ -1,55 +1,73 @@
-import style from './UpcomingScreenings.module.scss';
-import { screenings } from "../../../Data/screenings";
-import { movies } from "../../../Data/movies";
+"use client";
+
+import { useEffect, useState } from "react";
+import style from "./UpcomingScreenings.module.scss";
+
+type Movie = {
+  id: number;
+  Series_Title: string;
+  Poster_Link: string;
+};
+
+
+type Screening = {
+  id: number;
+  movieId: number;
+  auditorium: string;
+  startsAt: string;
+  movie?: Movie;
+};
 
 export default function UpcomingScreenings() {
-  const upcomingScreenings = screenings.map((screening) => {
-    const movie = movies.find((movie) => movie.id === screening.movieId);
+  const [upcomingScreenings, setUpcomingScreenings] = useState<Screening[]>([]);
 
-    return {
-      ...screening,
-      movie,
-    };
-  });
+  useEffect(() => {
+    async function fetchScreenings() {
+      const response = await fetch("/api/screenings");
+      const data = await response.json();
+      setUpcomingScreenings(data);
+    }
+
+    fetchScreenings();
+  }, []);
 
   return (
     <div className={style.upcomingScreenings}>
       <h2 className={style.upcomingScreeningsHeading}>Kommande visningar</h2>
 
       <div className={style.upcomingScreeningsTrack}>
- {upcomingScreenings.map((screening) => {
-  if (!screening.movie) return null;
+        {upcomingScreenings.map((screening) => {
+          if (!screening.movie) return null;
 
-  return (
-    <div className={style.upcomingScreeningsCard} key={screening.id}>
-      <img
-        className={style.upcomingScreeningsImg}
-        src={screening.movie.Poster_Link}
-        alt={screening.movie.Series_Title}
-      />
+          return (
+            <div className={style.upcomingScreeningsCard} key={screening.id}>
+              <img
+                className={style.upcomingScreeningsImg}
+                src={screening.movie.Poster_Link}
+                alt={screening.movie.Series_Title}
+              />
 
-      <h3 className={style.upcomingScreeningsTitle}>
-        {screening.movie.Series_Title}
-      </h3>
+              <h3 className={style.upcomingScreeningsTitle}>
+                {screening.movie.Series_Title}
+              </h3>
 
-      <p className={style.upcomingScreeningsTime}>
-        {new Date(screening.startsAt).toLocaleTimeString("sv-SE", {
-          hour: "2-digit",
-          minute: "2-digit",
+              <p className={style.upcomingScreeningsTime}>
+                {new Date(screening.startsAt).toLocaleTimeString("sv-SE", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </p>
+
+              <p className={style.upcomingScreeningsRoom}>
+                {screening.auditorium}
+              </p>
+            </div>
+          );
         })}
-      </p>
-
-      <p className={style.upcomingScreeningsRoom}>
-        {screening.auditorium}
-      </p>
-    </div>
-  );
-})}
       </div>
     </div>
   );
 }
-
 
 
 
