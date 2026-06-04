7b0d520de9735eb3b7673c87438160095029342d
Anders Eriksson <anteman.swe@gmail.com>
2026-06-04 18:46:34 +0200

    Switched up the styling in cookiepolicy page

--- kino-webb/src/app/cookiepolicy/page.tsx
+++ kino-webb/src/app/cookiepolicy/page.tsx
@@ -24,7 +24,7 @@ export default async function Cookiepolicy() {
           className={styles.cookiepolicy}
         ></div>
         <Link href="/" className={styles.homelink}>
-          Tillbaka till startsidan
+          &#10226; Tillbaka till startsidan
         </Link>
       </>
     );
