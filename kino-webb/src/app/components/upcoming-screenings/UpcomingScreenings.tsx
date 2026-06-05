9b677f67cb6fb2a9987d512e5b21e87c8b9e6f41
Anders Eriksson <anteman.swe@gmail.com>
2026-06-05 16:39:17 +0200

    Updated cards styling and some more info

--- kino-webb/src/app/components/upcoming-screenings/UpcomingScreenings.tsx
+++ kino-webb/src/app/components/upcoming-screenings/UpcomingScreenings.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import style from "./UpcomingScreenings.module.scss";
 import Image from "next/image";
+import Link from "next/link";
 
 type Movie = {
   id: number;
@@ -21,25 +22,25 @@ type Screening = {
 export default function UpcomingScreenings() {
   const [upcomingScreenings, setUpcomingScreenings] = useState<Screening[]>([]);
 
-useEffect(() => {
-  async function fetchScreenings() {
-   try {
-  const response = await fetch("/api/screenings");
+  useEffect(() => {
+    async function fetchScreenings() {
+      try {
+        const response = await fetch("/api/screenings");
 
-  if (!response.ok) {
-    setUpcomingScreenings([]);
-    return;
-  }
+        if (!response.ok) {
+          setUpcomingScreenings([]);
+          return;
+        }
 
-  const data = await response.json();
-  setUpcomingScreenings(data);
-} catch (error) {
-  setUpcomingScreenings([]);
-}
-  }
+        const data = await response.json();
+        setUpcomingScreenings(data);
+      } catch {
+        setUpcomingScreenings([]);
+      }
+    }
 
-  fetchScreenings();
-}, []);
+    fetchScreenings();
+  }, []);
 
   return (
     <div className={style.upcomingScreenings}>
@@ -49,38 +50,57 @@ useEffect(() => {
         {upcomingScreenings.map((screening) => {
           if (!screening.movie) return null;
 
-  return (
-    <div className={style.upcomingScreeningsCard} key={screening.id}>
-      <Image
-        className={style.upcomingScreeningsImg}
-        src={screening.movie.posterLink}
-        alt={screening.movie.seriesTitle}
-        width={200}
-        height={300}
-      />
+          return (
+            <Link
+              href={`/movies/${screening.movie.id}`}
+              className={style.upcomingScreeningsCard}
+              key={screening.id}
+            >
+              <Image
+                className={style.upcomingScreeningsImg}
+                src={screening.movie.posterLink}
+                alt={screening.movie.seriesTitle}
+                width={200}
+                height={300}
+              />
 
               <h3 className={style.upcomingScreeningsTitle}>
                 {screening.movie.seriesTitle}
               </h3>
 
-              <p className={style.upcomingScreeningsTime}>
-                {new Date(screening.startsAt).toLocaleTimeString("sv-SE", {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                })}
-              </p>
-
-              <p className={style.upcomingScreeningsRoom}>
-                {screening.auditorium}
-              </p>
-
+              <div className={style.infoContainer}>
+                <span className={style.upcomingScreeningsDay}>
+                  {new Date(screening.startsAt)
+                    .toLocaleString("sv-SE", {
+                      weekday: "long",
+                    })
+                    .toLocaleUpperCase()}
+                </span>
+                <span className={style.upcomingScreeningsDate}>
+                  {new Date(screening.startsAt)
+                    .toLocaleString("sv-SE", {
+                      day: "numeric",
+                      month: "short",
+                    })
+                    .toLocaleUpperCase()}
+                </span>
+                <span className={style.upcomingScreeningsTime}>
+                  {new Date(screening.startsAt).toLocaleTimeString("sv-SE", {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                  })}
+                </span>
+                <p className={style.upcomingScreeningsRoom}>
+                  {screening.auditorium.toLocaleUpperCase()}
+                </p>
+              </div>
               <Link
                 href={`/movies/${screening.movieId}/booking?screeningId=${screening.id}`}
                 className={style.bookingLink}
               >
                 Boka
               </Link>
-            </div>
+            </Link>
           );
         })}
       </div>
