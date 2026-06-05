37ce11d1483a544e669d7400a139219b2b31adbe
aisaksson187-create <aisaksson187@gmail.com>
2026-06-05 03:02:53 +0200

    Implemented booking links for both movie details and upcoming screenings

--- kino-webb/src/app/components/upcoming-screenings/UpcomingScreenings.tsx
+++ kino-webb/src/app/components/upcoming-screenings/UpcomingScreenings.tsx
@@ -1,7 +1,8 @@
-import style from './UpcomingScreenings.module.scss';
+import style from "./UpcomingScreenings.module.scss";
 import { screenings } from "../../../Data/screenings";
 import { movies } from "../../../Data/movies";
-import Image from 'next/image';
+import Image from "next/image";
+import Link from "next/link";
 
 export default function UpcomingScreenings() {
   const upcomingScreenings = screenings.map((screening) => {
@@ -18,41 +19,44 @@ export default function UpcomingScreenings() {
       <h2 className={style.upcomingScreeningsHeading}>Kommande visningar</h2>
 
       <div className={style.upcomingScreeningsTrack}>
- {upcomingScreenings.map((screening) => {
-  if (!screening.movie) return null;
+        {upcomingScreenings.map((screening) => {
+          if (!screening.movie) return null;
 
-  return (
-    <div className={style.upcomingScreeningsCard} key={screening.id}>
-      <Image
-        className={style.upcomingScreeningsImg}
-        src={screening.movie.Poster_Link}
-        alt={screening.movie.Series_Title}
-        width={200}
-        height={300}
-      />
+          return (
+            <div className={style.upcomingScreeningsCard} key={screening.id}>
+              <Image
+                className={style.upcomingScreeningsImg}
+                src={screening.movie.Poster_Link}
+                alt={screening.movie.Series_Title}
+                width={200}
+                height={300}
+              />
 
-      <h3 className={style.upcomingScreeningsTitle}>
-        {screening.movie.Series_Title}
-      </h3>
+              <h3 className={style.upcomingScreeningsTitle}>
+                {screening.movie.Series_Title}
+              </h3>
 
-      <p className={style.upcomingScreeningsTime}>
-        {new Date(screening.startsAt).toLocaleTimeString("sv-SE", {
-          hour: "2-digit",
-          minute: "2-digit",
+              <p className={style.upcomingScreeningsTime}>
+                {new Date(screening.startsAt).toLocaleTimeString("sv-SE", {
+                  hour: "2-digit",
+                  minute: "2-digit",
+                })}
+              </p>
+
+              <p className={style.upcomingScreeningsRoom}>
+                {screening.auditorium}
+              </p>
+
+              <Link
+                href={`/movies/${screening.movieId}/booking?screeningId=${screening.id}`}
+                className={style.bookingLink}
+              >
+                Boka
+              </Link>
+            </div>
+          );
         })}
-      </p>
-
-      <p className={style.upcomingScreeningsRoom}>
-        {screening.auditorium}
-      </p>
-    </div>
-  );
-})}
       </div>
     </div>
   );
 }
-
-
-
-
