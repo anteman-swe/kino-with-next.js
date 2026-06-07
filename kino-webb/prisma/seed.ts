39cba698ce854be96e4aab69214e167939809ee4
Anders Eriksson <anteman.swe@gmail.com>
2026-06-07 14:44:45 +0200

    Containers now starting on prod:setup

--- kino-webb/prisma/seed.ts
+++ kino-webb/prisma/seed.ts
@@ -14,29 +14,7 @@ const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 async function main() {
-  console.log("Tömmer databasen...");
-  await prisma.$executeRawUnsafe(
-    `TRUNCATE TABLE "Review" RESTART IDENTITY CASCADE;`, // Depends on movies and users
-  );
-  await prisma.$executeRawUnsafe(
-    `TRUNCATE TABLE "Booking" RESTART IDENTITY CASCADE;`, // Depends on screenings and users
-  );
-  await prisma.$executeRawUnsafe(
-    `TRUNCATE TABLE "BookingSeat" RESTART IDENTITY CASCADE;`,
-  );
-  await prisma.$executeRawUnsafe(
-    `TRUNCATE TABLE "Screening" RESTART IDENTITY CASCADE;`, // Depends on movies
-  );
-  await prisma.$executeRawUnsafe(
-    `TRUNCATE TABLE "Movie" RESTART IDENTITY CASCADE;`,
-  );
-  await prisma.$executeRawUnsafe(
-    `TRUNCATE TABLE "User" RESTART IDENTITY CASCADE;`,
-  );
-  await prisma.$executeRawUnsafe(
-    `TRUNCATE TABLE "Offer" RESTART IDENTITY CASCADE;`,
-  );
-  console.log("Databasen är rensad och nollställd! Börjar seeda...");
+  console.log("Börjar seeda databasen...");
 
   console.log("Genererar haschade lösenord...");
   const hashedPassword = await saltAndHashPassword("secret1234");
